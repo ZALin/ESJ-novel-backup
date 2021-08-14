220a6f63aeef9b23f9b835f3797becdf504dc0d2
ZALin <andy03031193@gmail.com>
2021-08-14 08:00:38 +0800

    add each chapter's author

--- esjbackup.py
+++ esjbackup.py
@@ -9,14 +9,15 @@ def write_page(url, dst_file):
     r = requests.get(url)
     html_element = lxml.html.document_fromstring(r.text)
     title = html_element.xpath('//h2')[0]
+    author = html_element.xpath('//div[@class="single-post-meta m-t-20"]/div')[0]
     content = html_element.xpath('//div[@class="forum-content mt-3"]')[0]
     with open(dst_file, 'a') as f:
-        f.write(title.text_content().encode('utf-8')+'\n')
+        f.write('[' + title.text_content().encode('utf-8') + '] ' + author.text_content().strip().encode('utf-8') + '\n')
         f.write(content.text_content().encode('utf-8')+'\n\n')
 
 if __name__ == "__main__":
 
-    novel_id = '1619837711'  # the novel id which you want to download
+    novel_id = ''  # the novel id which you want to download
     r = requests.get('https://www.esjzone.cc/detail/' + novel_id + '.html')
     html_element = lxml.html.document_fromstring(r.text)
 
